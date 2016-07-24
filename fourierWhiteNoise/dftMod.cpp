ef98a733906da1a9526f88a65a8e7a50a57f984d
Marcus Plutowski <Achierius@gmail.com>
2016-07-24 08:38:04 -0700

    Fix namespace bugs in dftMod.cpp

--- fourierWhiteNoise/dftMod.cpp
+++ fourierWhiteNoise/dftMod.cpp
@@ -1,22 +1,25 @@
 #include "dftMod.h"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/mat.hpp"
 
 
+void siganal::visualizeFDomain(cv::Mat& targetMatrix){
+    cv::cvtColor(targetMatrix, targetMatrix, CV_BGR2GRAY) ;
 
-void sigAnal::visualizeFDomain(cv::Mat& targetMatrix){
-    cvtColor(targetMatrix, targetMatrix, CV_BGR2GRAY) ;
+    cv::copyMakeBorder(targetMatrix, targetMatrix, 0, cv::getOptimalDFTSize(targetMatrix.rows)-targetMatrix.rows, 0, cv::getOptimalDFTSize(targetMatrix.cols)-targetMatrix.cols, cv::BORDER_CONSTANT, cv::Scalar::all(0));
 
-    copyMakeBorder(targetMatrix, targetMatrix, 0, getOptimalDFTSize(targetMatrix.rows)-targetMatrix.rows, 0, getOptimalDFTSize(targetMatrix.cols)-targetMatrix.cols, BORDER_CONSTANT, Scalar::all(0));
+    cv::Mat planes[] = {cv::Mat_<float>(targetMatrix), cv::Mat::zeros(targetMatrix.size(), CV_32F)}; 
+    cv::Mat complexIm;
+    cv::merge(planes, 2, complexIm);
 
-    Mat planes[] = {Mat_<float>(im), Mat::zeros(im.size(), CV_32F)}; 
-    Mat complexIm;
-    merge(planes, 2, complexIm);
+    cv::dft(complexIm, complexIm);
 
-    dft(complexI, complexIm);
+    cv::split(complexIm, planes);
+    cv::magnitude(planes[0], planes[1], targetMatrix);
 
-    split(complexIm, planes);
-    magnitude(planes[0], planes[1], targetMatrix);
-
-    targetMatrix += Scalar::all(1);
-    log(targetMatrix, targetMatrix);
-    normalize(targetMatrix, targetMatrix, 0, 1, CV_MINMAX);
+    targetMatrix += cv::Scalar::all(1);
+    cv::log(targetMatrix, targetMatrix);
+    cv::normalize(targetMatrix, targetMatrix, 0, 1, CV_MINMAX);
 }
