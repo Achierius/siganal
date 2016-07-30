55b2f4a0dfc8f7e76b54ae8171ba32957cd5c4c1
Marcus Plutowski <Achierius@gmail.com>
2016-07-30 13:57:21 -0700

    Displays a colorized output of the DFT
    Blue is phase, red is magnitude, it's so pretty~

--- fourierWhiteNoise/fourierColorizeTest.cpp
+++ fourierWhiteNoise/fourierColorizeTest.cpp
@@ -7,14 +7,16 @@
 #include <sstream>
 #include <cmath>
 #include <complex>
+#include "dftMod.h"
 using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv){
    
-    int c;
+    int c = 1;
 
     Mat image = imread("./duck.jpg", 1);
+    cout<<siganal::typeID(image.type())<<"\n";
     cvtColor(image, image, CV_BGR2GRAY);
 
     int optW = getOptimalDFTSize(image.cols); 
@@ -22,21 +24,43 @@ int main(int argc, char** argv){
     copyMakeBorder(image, image, 0, optH-image.rows, 0, optW-image.cols, BORDER_CONSTANT, Scalar::all(0));
 
     Mat planes[] = {Mat_<float>(image), Mat::zeros(image.size(), CV_32F)};
-    Mat_<float> complexIm(image);
-    //merge(planes, 2, complexIm); //2-channel image time
+    Mat complexIm;
 
-    dft(complexIm, complexIm); //Uh... I think because the second channel is all zeroes, it doesn't affect the DFT?
-    cout<<complexIm.at<float>(2,2)<<endl;
+    merge(planes, 2, complexIm);
+    dft(complexIm, complexIm);
+    split(complexIm, planes);
 
+    Mat magnitudeMat;
+    Mat phaseMat;
+    magnitude(planes[0], planes[1], magnitudeMat);
+    phase(planes[0], planes[1], phaseMat);
 
-    Mat complexIm2;
+    magnitudeMat += Scalar::all(1);
+    phaseMat += Scalar::all(1);
+    log(magnitudeMat, magnitudeMat);
+    log(phaseMat, phaseMat);
+    normalize(magnitudeMat, magnitudeMat, 0, 1, CV_MINMAX);
+    normalize(phaseMat, phaseMat, 0, 1, CV_MINMAX);
 
+    Mat outputImage;
+    Mat imArray[] = {Mat_<float>(phaseMat), Mat::zeros(magnitudeMat.size(), CV_32F), Mat_<float>(magnitudeMat)};
+    /*cout
+        <<"MatCharImag: "<<imArray[0].size<<" "<<imArray[0].depth()<<", \n"
+        <<"MatCharReal: "<<imArray[2].size<<" "<<imArray[2].depth()<<", \n"
+        <<"MatCharZeros: "<<imArray[1].size<<" "<<imArray[1].depth()<<".\n";
+    cout<<"CV8U: "<<CV_8U<<", \n"
+        <<"CV8S: "<<CV_8S<<", \n"
+        <<"CV16U: "<<CV_16U<<", \n"
+        <<"CV16S: "<<CV_16S<<", \n";*/
+    merge(imArray, 3, outputImage);
+     
+    namedWindow("output");
 
-    merge(planes, 2, complexIm2);
+    cout<<siganal::typeID(outputImage.type())<<"\n";
+    while(c != 0){
+       imshow("output", outputImage);
+        c = waitKey(0);
+    }
 
-    dft(complexIm2, complexIm2);
-
-    split(complexIm2, planes);
-    cout<<planes[1].at<float>(2,1)<<endl;
-  return 0;
+    return 0;
 }
