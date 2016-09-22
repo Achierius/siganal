6bdf42afa4f9f5c4cae5f6770c7cf0976f686035
Marcus Plutowski <Achierius@gmail.com>
2016-09-22 15:27:47 -0700

    Complete header file for the kalman filter class

---
+++ kalman/kalmanFilter.h
@@ -0,0 +1,50 @@
+#ifndef __SIGANAL_KALMAN_FILTER_H
+#define __SIGANAL_KALMAN_FILTER_H
+
+#include "./eigen/Eigen/Dense"
+#include <string>
+
+class KalmanFilter{
+    using Eigen::VectorXd;
+    using Eigen::MatrixXd;
+
+public:
+    KalmanFilter();
+    KalmanFilter(VectorXd initialState, MatrixXd initialStateCV, MatrixXd stateTransition, MatrixXd sensorMap, MatrixXd controlMap, MatrixXd noiseCV, MatrixXd sensorCV);
+
+    initialize(VectorXd initialState, MatrixXd initialStateCV, MatrixXd stateTransition, MatrixXd sensorMap, MatrixXd controlMap, MatrixXd processNoiseCV, MatrixXd sensorNoiseCV);
+
+    void setQ(MatrixXd newProcessNoiseCV);
+    void setR(MatrixXd newSensorNoiseCV);
+    void setF(MatrixXd newF);
+    void setB(MatrixXd newB);
+    void setH(MatrixXd newH);
+
+    void setTimeStep(double seconds);
+    double getTimeStep();
+    double getTimeElapsed();
+
+    void updateFilter(VectorXd sensorInput, VectorXd controlInput);
+
+    VectorXd getCurrentEstimate();
+    MatrixXd getCurrentCovariance();
+private:
+    MatrixXd k_F; //State Transition Matrix
+    MatrixXd k_B; //Control Input Translation Matrix
+    MatrixXd k_H; //Observation Space Translation Matrix
+
+    MatrixXd k_Pcur; //Current Covariance of the State Estimate
+    MatrixXd k_Ppre; //Previous ''
+    MatrixXd k_R; //Covariance of the Observation Noise
+    MatrixXd k_Q; //Covariance of the Process Noise
+
+    VectorXd k_xcur; //Current State Estimate
+    VectorXd k_xpre; //Previous State Estimate
+    VectorXd k_u; //Current control input
+    VectorXd k_z; //Current Observation
+
+
+    double timeStep;
+    double timeElapsed;
+};
+
