ae31dfaec263c53022b91db9244cbb6d5f98d714
Marcus Plutowski <achierius@gmail.com>
2016-09-30 02:33:33 +0000

    Rescale air resistance to be proportional to the first power when velocity < 10

--- kalman/accelCar.cpp
+++ kalman/accelCar.cpp
@@ -4,7 +4,8 @@
 #include "./eigen/Eigen/Dense"
 #include <stdlib.h> //rand
 #include <time.h> //rand
-
+#include <math.h> //erf
+#include <random>
 /*
  * Just a flywheel spinning at some random speed, blank test for kalman.
  */
@@ -21,38 +22,20 @@ using namespace std;
 string RED= "\033[31m";
 string CLOSE = "\e[0m";
 
-double erf(double x){ //Error Function!
-    return 1-1/(pow((1+0.278393*pow(x,1)+0.230389*pow(x,2)+0.000972*pow(x,3)+0.078108*pow(x,4)),4));
-}
-double cdf(double x, double mean, double stdev){
-    return 0.5*(1+erf((x-mean)/(1.41421356*stdev)));
-}
-double lazyGauss(double mean, double stdev){ //Whooo cental limit theorem
-    srand(time(NULL));
-    /*double destination = (rand()%99+1)/100;
-    if(destination > 0.5){
-	for(double i = 0; true; i+=0.005){
-	    if(abs(cdf(i, mean, stdev)-destination)<0.01){return i;}
-	}
-    }
-    else if(destination < 0.5){
-	for(double i = 0; true; i-=0.005){
-	    if(abs(cdf(i, mean, stdev)-destination)<0.01){return i;}
-	}
-    }
-    else if(destination = 0.5){
-	return 0;
-    }*/    
-    return mean;
-}
 void updateSystem(double dT, double accel){
-    trueX = trueX + trueV*dT + trueA*dT*dT/2 + lazyGauss(0, 0.75)*dT;
-    trueV = trueV + trueA*dT + lazyGauss(0, 1.50)*dT - lazyGauss(0-pow(trueV,2)/100, trueV/10)*dT;
+    normal_distribution<double> theWind(0, 1);
+    std::default_random_engine generator;
+    normal_distribution<double> airResistance(abs(trueV)>10 ? pow(trueV,2)/100 : trueV/100, trueV/10);
+
+    trueX = trueX + trueV*dT + trueA*dT*dT/2 - theWind(generator)*dT;
+    trueV = trueV + trueA*dT - airResistance(generator)*dT*(trueV/abs(trueV));
     trueA = accel;
 }
 VectorXd measureSystem() 
 {
-    VectorXd ret(3); ret << 0, 0, trueA+lazyGauss(0, 1);
+    normal_distribution<double> sensorNoise(0, 0.3);
+    std::default_random_engine generator;
+    VectorXd ret(3); ret << 0, 0, trueA+sensorNoise(generator)*dT;
     return ret;
 }
 double returnInput(double timeElapsed){
@@ -87,9 +70,8 @@ int main(){
                                0, 0, 0,
 			       0, 0, 1;
    
-	cout<<"Pre-Kalman\n";
     KalmanFilter kalman(stateVec, k_P, stateTransition, k_H, k_B, k_Q, k_R, dT);
-	cout<<"Post-Kalman\n";
+
     double errorX = 1;
     double errorV = 1;
     int i; 
@@ -101,18 +83,16 @@ int main(){
     int stAn = 0;
 
     for(i = 0; stX == 0 || stV == 0; i++){
-	cout<<"StartLoop\n";
         k_u << returnInput(timeElapsed);
-	cout<<"ReturnInput\n";
         updateSystem(dT, k_u(0));
-	cout<<"UpdatedSystem\n";
         kalman.updateFilter(measureSystem(), k_u);
-	cout<<"UpdatedFilter\n";
 	timeElapsed+=dT;
-	std::cout<<"Time: "<<i*dT<<endl;
-	std::cout<<"Estimates: Position: "<<RED<<kalman.getCurrentEstimate()(0)<<CLOSE<<" Velocity: "<<RED<<kalman.getCurrentEstimate()(1)<<CLOSE<<" Acceleration: "<<RED<<kalman.getCurrentEstimate()(2)<<CLOSE<<endl;
-	std::cout<<"Actuals: Position: "<<RED<<trueX<<CLOSE<<" Velocity: "<<RED<<trueV<<CLOSE<<" Acceleration: "<<RED<<trueA<<CLOSE<<endl;
-	std::cout<<endl;
+	if(i%50 == 0){
+		std::cout<<"Time: "<<i*dT<<endl;
+		std::cout<<"Estimates: Position: "<<RED<<kalman.getCurrentEstimate()(0)<<CLOSE<<" Velocity: "<<RED<<kalman.getCurrentEstimate()(1)<<CLOSE<<" Acceleration: "<<RED<<kalman.getCurrentEstimate()(2)<<CLOSE<<endl;
+		std::cout<<"Actuals: Position: "<<RED<<trueX<<CLOSE<<" Velocity: "<<RED<<trueV<<CLOSE<<" Acceleration: "<<RED<<trueA<<CLOSE<<endl;
+		std::cout<<endl;
+	}
     }
 }
 	
