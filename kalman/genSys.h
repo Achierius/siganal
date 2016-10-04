0ca551946b0006145ab2950a00498735af4f5742
Marcus Plutowski <achierius@gmail.com>
2016-10-04 18:01:18 +0000

    Create header file for upcoming general system simulator
    It's essentially an explicit discrete-time variant of a state space representation of a system, with one thing added: each timestep,
    it calls a function (the function pointed to by nonlinearStateComponent) and adds the result to the state. This allows for non-linear systems,
    such as those affected by air resistance.

---
+++ kalman/genSys.h
@@ -0,0 +1,46 @@
+#ifndef __SIGANAL_GENSYS_H
+#define __SIGANAL_GENSYS_H
+
+#include "./eigen/Eigen/Dense"
+
+class GenSys{
+using namespace Eigen;
+
+public:
+    GenSys(VectorXd initState, stateTran, sNoise, controlTran, measTran, VectorXd (*nonLinCom)(VectorXd, VectorXd));
+
+    void setState(VectorXd s);
+    void setStateTransition(MatrixXd sT);
+
+    void setSNoiseSD(VectorXd q);
+    void setMNoiseSD(VectorXd r);
+
+    void setControlTranslation(MatrixXd b);
+    void setMeasurementTranslation(MatrixXd h); 
+
+    VectorXd getState();
+    VectorXd getMeasurement();
+
+    void setdT(double dT);
+
+    void updateFilter(VectorXd controlInput);
+private:
+    MatrixXd stateTransition;
+    VectorXd state;
+    VectorXd statePrev; //In case the nonlinear function component needs this
+    unsigned int states;
+    VectorXd sNoiseSD;
+
+    VectorXd (*nonlinearStateComponent)(VectorXd, VectorXd);
+
+    MatrixXd controlTranslation;
+    VectorXd control;
+
+    MatrixXd measurementTranslation;
+    VectorXd measurement;
+    VectorXd mNoiseSD;
+
+    double dT;
+};
+
+#endif//__SIGANAL_GENSYS_H
