6f7b99fd9b24963d6671a409104c4f65cb3b78b7
Marcus Plutowski <achierius@gmail.com>
2016-10-05 04:40:17 +0000

    Implement getters/setters for genSys

---
+++ kalman/genSys.cpp
@@ -0,0 +1,50 @@
+#include "genSys.h"
+
+
+GenSys::GenSys(VectorXd initState, stateTran, sNoise, controlTran, measTran, VectorXd (*nonLinCom)(VectorXd, VectorXd), double DT){
+    setState(initState);
+    statePrev = initState;
+    setStateTransition(stateTran);
+    setSNoiseSD(sNoise);
+    setControlTranslation(controlTran);
+    setMeasurementTranslation(measTran);
+    nonlinearStateComponent = nonLinCom;
+    setdT(DT);
+}
+
+void GenSys::setState(VectorXd s){
+    state = s;
+    states = s.rows();
+}
+void GenSys::setStateTransition(MatrixXd sT){
+    stateTransition = sT;
+}
+
+void GenSys::setSNoiseSD(VectorXd q){
+    sNoiseSD = q;
+}
+void GenSys::setMNoiseSD(VectorXd r){
+    mNoiseSD = r;
+}
+
+void GenSys::setControlTranslation(MatrixXd b){
+    controlTranslation = b;
+}
+void GenSys::setMeasurementTranslation(MatrixXd h){
+    measurementTranslation = h;
+} 
+
+VectorXd GenSys::getState(){
+    return state;
+}
+VectorXd GenSys::getMeasurement(){
+    return measurement;
+}
+
+void GenSys::setdT(double dT){
+    this->dT = dT;
+}
+
+void GenSys::updateFilter(VectorXd controlInput){
+
+}
