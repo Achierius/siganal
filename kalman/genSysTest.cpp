d5628fcf9360e9f61c0b84e921038dd0056457b7
Marcus Plutowski <achierius@gmail.com>
2016-10-08 17:13:35 +0000

    Successfully test general system simulator

---
+++ kalman/genSysTest.cpp
@@ -0,0 +1,32 @@
+#include "genSys.h"
+#include <iostream>
+#include <string>
+#include "./eigen/Eigen/Dense"
+
+using namespace Eigen;
+
+VectorXd airResistance(VectorXd state, VectorXd prevState){
+	VectorXd ret(2); ret << 0, 0;
+	return ret;
+}
+VectorXd controlInput(double time){
+	VectorXd ret(1); ret<<0;
+	//if(time<3) ret(1) = 1;
+	return ret;
+}
+int main(){
+	double dT = 0.01;
+	VectorXd state(2); state << 0, 1; //x, x'
+	MatrixXd st(2,2); st << 1, dT, 0, 1;
+	VectorXd q(2); q << 0.0001, 0.0001;
+	VectorXd r(2); r = q;
+        MatrixXd h(2,2); h << 1, 0, 0, 0;
+	MatrixXd b(2,1); b << dT*dT/2, dT;
+	
+	GenSys sys(state, st, q, b, h, r, &airResistance, dT);
+
+	for(int i = 0; i < 10000; i++){
+		sys.updateFilter(controlInput(i*dT));
+		if(i%500==0) std::cout<<sys.getMeasurement()(0)<<std::endl;
+	}
+}
