b0988ed0086efadb8cf610f7280898cdfec364fc
Marcus Plutowski <achierius@gmail.com>
2017-05-06 03:28:47 +0000

    Complete header for UKF implementation

--- kalman/ukf/ukf.hpp
+++ kalman/ukf/ukf.hpp
@@ -1,38 +1,74 @@
-#ifndef __MASS_UKF_HPP
-#define __MASS_UKF_HPP
+	#ifndef __MASS_UKF_HPP
+	#define __MASS_UKF_HPP
 
-#include <iostream>
-#include <cmath>
-#include <ctime>
-#include <cstdio>
+	#include <iostream>
+	#include <cmath>
+	#include <ctime>
+	#include <cstdio>
 #include "./eigen/Eigen/Dense"
-#include "./include/vFunc.hpp"
+#include "./vFunc/vFunc.hpp"
 
 namespace ukf_mass{
 
   class ukf{
     public:
-      ukf(int states, 
+      ukf(int states, int measurements, double alpha, double beta, double kappa, double dT,   //Full definition of the UKF
+          Eigen::VectorXd initState, Eigen::MatrixXd initCovar, vFunc transform, vFunc measure,
+          Eigen::MatrixXd Q, Eigen::MatrixXd R);
+      ukf(int states, int measurements, double alpha, double beta, double dT,                 //Assumes that Kappa=0
+          Eigen::VectorXd initState, Eigen::MatrixXd initCovar, vFunc transform, vFunc measure,
+          Eigen::MatrixXd Q, Eigen::MatrixXd R);
+      ukf(int states, int measurements, double alpha, double beta, double dT,                 //Assumes that Covar = I, initial state -> [0,...,0]^T
+          vFunc transform,
+          Eigen::MatrixXd Q, Eigen::MatrixXd R);
+
+      ~ukf();
+
+      ukf(const ukf&) = delete;
+      ukf& operator = (const ukf&) = delete;
+
+      void stepUKF(double dT, Eigen::VectorXd measurement, Eigen::VectorXd control);
+      void stepUKF(double dT);								      //Assumes measuremnt && control == [0...0]^T
+
+      void setQ(Eigen::MatrixXd Q);
+      void setR(Eigen::MatrixXd R);
+
+      void setTransform(vFunc transform);
+      void setMeasure(vFunc measure);
+
+      Eigen::VectorXd currentState();
+
     private:
       int _states;
       int _measurements;
 
+      double _alpha; //Spread of sigma-points
+      double _beta;  //Information about prior distribution; gaussian : = 2
+      double _kappa; //Uh, usually 0
+
+      double _lambda;           //alpha^s * (states + kappa) - L
+      double _mean0;            //lambda / (L+lambda)
+      double _covar0;           // lambda / (L + lambda)  + 1 - alpha^2 + beta
+      double _statistic;        //Mean at t =/= 0  =  covar at t =/= 0  =  1/(2*(L+lambda))
+
+      Eigen::MatrixXd _sigmaPoints; //Yeah, see the paper lmao
+      ukf_mass::vFunc _transform;   //Moves from time k-1 to time k across a given dT
+      ukf_mass::vFunc _measure;
+
+      Eigen::MatrixXd _sqrtP;   //sqrt of a matrix, in this case, sqrt*sqrt^T
+      Eigen::MatrixXd _Pkyy;
+      Eigen::MatrixXd _Pkxy;    //lolwtf
+      Eigen::MatrixXd _Kk;	//Kalman Gain
+
+      Eigen::MatrixXd _Q;       //Covariance of the process noise
+      Eigen::MatrixXd _R;       //Covariance of the measurement noise
+      
+      Eigen::MatrixXd _covar;
+      Eigen::MatrixXd _prevCovar;
       Eigen::VectorXd _state;
       Eigen::VectorXd _prevState;
 
-      Eigen::MatrixXd _stateCovariance;
-      Eigen::MatrixXd _prevStateCovariance;
-
-      Eigen::MatrixXd _sensorCovariance;
-      Eigen::MatrixXd _processCovariance;
-
-      Eigen::MatrixXd _K;
-      Eigen::MatrixXd _H;
-      Eigen::MatrixXd _
-
-
-
-  }
+  };
 
 }
 
