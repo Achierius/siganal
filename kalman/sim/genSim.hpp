84f646fc98a87f9b5b9ae541ca0e6aebbbcf6fd8
Marcus Plutowski <achierius@gmail.com>
2017-07-01 20:41:59 +0000

    Create a class for representing general system simulators
    Based very loosely off of a state space representation. Allows for full
    nonlinearity in really any variable at any  point, but represents noise
    as a secondary optional convolution rather than a core part of the state
    transition. Pretty sure it's generally time invariant, I think. Don't really
    know how the whole feedforward thing works.
    
    Still need to implement the pre-defined regulatory functions. Are things
    supposed to be protected rather than prviate? I think so.

---
+++ kalman/sim/genSim.hpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "./eigen/Eigen/Dense"
+#include <string>
+#include <chrono>
+#include <cmath>
+#include <ctime>
+
+
+/*
+ ***** GenSim.hpp *****
+ *
+ * General System Representation
+ * Assumptions: Time invariant system transition
+ *
+ * Represents the system with a single state vector. Transitions consist of
+ * application by both the previous state and a control input vector 
+ * simultaneously. Transition may be non-linear.
+ *
+ * Two stages of the transition:
+ *  -Physical Transition
+ *  -Measurement
+ * The latter of which represents the measured output of
+ * the system ('y' vector). Measurement may be nonlinear.
+ * 
+ * System noise applied in 'measurement' and 'process' noise segments.
+ * Both types of noise may be Non-Gaussian/White/Zero Mean, and may
+ * vary with the system state.
+ *
+ ***** GenSim.hpp *****
+ */
+
+namespace ukf_mass{
+
+class GenSim{
+public:
+  using state Eigen::VectorXd;
+  using ms std::chrono::milliseconds;
+  state getCurrentState(bool measure, bool mNoise);
+  state updateState(state controlInput, double duration, bool pNoise);
+  
+protected:
+  int   _states;  //Number of state variables
+  int   _outputs; //Number of output variables [from _measurement(~)]
+  state _state;   //Current system state
+  state _input;   //Most recent control input
+  ms    _dt;      //
+
+  state _measurement(state input, state measurement, bool feedforward, ms dT) = 0;    //
+  state _transition(state input, state measurement, ms dT) = 0;                       //
+
+  state _pNoise(state state) = 0;                                                     //Applies noise to state provided. Returns _states x 1 vector.
+  state _mNoise(state measurement, state state) = 0;                                  //Applies noise to the measurement provided, presumably from the _measurement function.
+                                                                                      //cont. Returns `_outputs x 1` size vector. May vary noise with actual system state.
+  
+};
+
+}
+
